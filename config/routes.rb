c31b969297bfd5d78d0f43e28035d4ddc495a86a
christycui <christycui14@gmail.com>
2015-03-05 22:00:41 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Myflix::Application.routes.draw do
+  root to: 'videos#show'
+  get '/home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
+  resources :videos, only: [:show]
+  resources :categories, only: [:show]
 end
