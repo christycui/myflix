21c75b0c03dac5de280514d367012e64b6f68470
christycui <christycui14@gmail.com>
2015-03-06 18:13:21 +0000

    testing category and video models

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Video, :type => :model do 
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
+end
