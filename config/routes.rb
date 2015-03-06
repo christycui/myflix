be98c6399d45a1311c2946452df2426a60439289
christycui <christycui14@gmail.com>
2015-03-06 21:44:44 +0000

    implement search in UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Myflix::Application.routes.draw do
   root to: 'videos#index'
   get '/home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
-  resources :videos, only: [:show]
+  resources :videos, only: [:show] do
+    collection do
+      get '/search', to: 'videos#search'
+    end
+  end
   resources :categories, only: [:show]
 end
