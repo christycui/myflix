47139e071afd58207e07372fbe71f9bf2824e638
christycui <christycui14@gmail.com>
2015-03-25 02:35:40 +0000

    feature tests

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "user logs in" do
+  
+  scenario "user logs in with correct credentials" do
+    user = Fabricate(:user)
+    sign_in(user)
+    expect(page).to have_content(user.full_name)
+  end
+  
+  scenario "user fails to log in because of incorrect credentials" do
+    visit login_path
+    fill_in "email_address", :with => Faker::Internet.email
+    fill_in "password", :with => Faker::Lorem.word
+    click_button("Sign In")
+    expect(page).to have_content('Sign In')
+    expect(page).to have_content('Invalid email or password')
+  end
+ 
+end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -5,3 +5,10 @@ end
 def clear_current_user
   session[:user_id] = nil
 end
+
+def sign_in(user=nil)
+  visit login_path
+  fill_in("email_address", :with => user.email_address)
+  fill_in("password", :with => user.password)
+  click_button('Sign In')
+end
