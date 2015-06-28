aae7b3a38d2408e79ed9856bc074c68e972cb32e
christycui <christycui14@gmail.com>
2015-06-28 18:07:54 +0000

    completed social networking

--- spec/features/user_following_spec.rb
+++ spec/features/user_following_spec.rb
@@ -3,14 +3,23 @@ require 'rails_helper'
 feature 'User following' do
   scenario 'user follows another user' do
     user = Fabricate(:user)
-    another_user = Fabricate(:another_user)
+    another_user = Fabricate(:user)
     video = Fabricate(:video)
     review = Fabricate(:review, video: video, user: another_user)
     
     sign_in(user)
     visit video_path(video)
-    click_link another_user.name
+    click_link another_user.full_name
+    click_link 'Follow'
+    expect(page).to have_css('button', text: 'Following')
+    click_link 'People'
+    expect(page).to have_link(another_user.full_name)
+    
+    unfollow
+    expect(page).not_to have_link(another_user.full_name)
   end
   
-  scenario 'user unfollows anther user'
+  def unfollow
+    find("a[rel='nofollow']").click
+  end
 end
